691a6a2a979b0cf37bab3d9aaf4d8b8b2121c62a
Emilie Gao <emiliegao@berkeley.edu>
2016-03-08 20:34:14 -0800

    hellz ya

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -9,13 +9,63 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    @IBOutlet weak var hangmanImage: UIImageView!
+    
+    @IBOutlet weak var guessStatus: UILabel!
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var incorrectGuessLabel: UILabel!
+    
+    var phraseGuess:String = ""
+    
+    var incorrectChars:[Character] = []
+    
+    var incorrectGuessString:String = "Incorrect Guesses: "
+    
+    var incorrectCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        let phrase = hangmanPhrases.getRandomPhrase()
+        
+        incorrectCount = incorrectChars.count
+        if (incorrectCount == 0) {
+            hangmanImage.image = UIImage(named: "hangman1.gif")
+        } else if (incorrectCount == 1) {
+            hangmanImage.image = UIImage(named: "hangman2.gif")
+        } else if (incorrectCount == 2) {
+            hangmanImage.image = UIImage(named: "hangman3.gif")
+        } else if (incorrectCount == 3) {
+            hangmanImage.image = UIImage(named: "hangman4.gif")
+        } else if (incorrectCount == 4) {
+            hangmanImage.image = UIImage(named: "hangman5.gif")
+        } else if (incorrectCount == 5) {
+            hangmanImage.image = UIImage(named: "hangman6.gif")
+        } else if (incorrectCount == 6) {
+            hangmanImage.image = UIImage(named: "hangman7.gif")
+        }
+
+        
+        for i in phrase.characters.indices {
+            if (phrase[i] == " ") {
+                phraseGuess += " "
+            } else {
+                phraseGuess += "_"
+            }
+        }
+        
+        guessStatus.text = phraseGuess
+
+        for i in incorrectChars {
+            incorrectGuessString += String(i)
+        }
+        
+        incorrectGuessLabel.text = incorrectGuessString
+        
         print(phrase)
     }
 
@@ -24,7 +74,26 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func correctButtonPressed(sender: AnyObject) {
+        var currGuess = guessStatus.text
+        if (currGuess!.characters.last == " ") {
+            currGuess =
+                String(currGuess!.characters.dropLast())
+        } else {
+            currGuess = String(currGuess!.characters.dropLast())
+        }
+        
+        guessStatus.text = currGuess
+        
+    }
+    
+    @IBAction func incorrectButtonPressed(sender: AnyObject) {
+        
+    }
+    
+    
+    
+        
     /*
     // MARK: - Navigation
 
